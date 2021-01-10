df3342eb43cfda35ac4891afa5d4d07c23cbd898
khamatullinrv <khamatullinrv@yandex.ru>
2021-01-10 17:02:08 +0500

    4.1.0. Оператор if с блоком else [#425825] Задание
    1. Разработайте программу для кинотеатра. В кинотеатре показывают фильмы для взрослых.

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class Cinema {
     public static void access(int age) {
         System.out.println("The age of the customer is : " + age);
-        if (...) {
+        if ( age >= 21) {
             System.out.println("Welcome to the cinema.");
         } else {
             System.out.println("It is not for you.");
@@ -13,6 +13,10 @@ public class Cinema {
     public static void main(String[] args) {
         Cinema.access(21);
         Cinema.access(16);
-
+        Cinema.access(17);
+        Cinema.access(18);
+        Cinema.access(19);
+        Cinema.access(20);
+        Cinema.access(21);
     }
 }
