7f9c99d9fd3fe2f0092c838b51f38564c3f524f4
khamatullinrv <khamatullinrv@yandex.ru>
2021-01-10 16:36:01 +0500

    4.1. Операторы ветвлений [#425840]

--- src/ru/job4j/condition/AlertDivByZero.java
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -1,4 +1,19 @@
 package ru.job4j.condition;
 
 public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(-3);
+    }
+
+    public static void possibleDiv(int number) {
+       // boolean result = number == 0;
+        //if (result) {
+        //    System.out.println("Could not div by 0.");
+        //}
+        boolean result= number < 0 ; {
+            if (result)
+            System.out.println("This is negative numbers.");
+        }
+    }
 }
---
+++ test/ru/job4j/calculate/X2Test_2.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class X2Test_2 {
+
+    @Test
+    public void calc() {
+    }
+}
