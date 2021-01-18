24092b2972fede59275406ca6a10ece03d3a4ef7
khamatullinrv <khamatullinrv@yandex.ru>
2021-01-19 00:27:36 +0500

    4.4. Вложенный оператор if else [#425839]

---
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MultiMaxTest {
+
+    @Test
+    public void whenSecondMax() {
+        int result = MultiMax.max(1, 4, 2);
+        assertThat(result, is(4));
+}
