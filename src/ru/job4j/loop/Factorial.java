f1f9aa1c8b38161cd979946fb65a74c52f3dbee7
khamatullinrv <khamatullinrv@yandex.ru>
2021-01-22 20:01:38 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне [#425943]

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -11,6 +11,6 @@ public class Factorial {
     }
 
     public static void main(String[] args) {
-        System.out.println(Factorial.calc(33));
+        System.out.println(Factorial.calc(0));
     }
 }
