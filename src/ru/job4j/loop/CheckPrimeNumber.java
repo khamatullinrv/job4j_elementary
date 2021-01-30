8938920fce74740f04eb39423cce235be7bbb851
khamatullinrv <khamatullinrv@yandex.ru>
2021-01-30 22:44:39 +0500

    5.6. Простые числа [#425936]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -15,7 +15,7 @@ public class CheckPrimeNumber {
     public static void main(String[] args) {
             System.out.println(check(5));
             System.out.println(check(4));
-             System.out.println(check(1));
+             System.out.println(check(3));
 
     }
 }
