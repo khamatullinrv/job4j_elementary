f2a323a0610097a218d19493c899a499d17b5f2e
khamatullinrv <khamatullinrv@yandex.ru>
2021-02-23 19:06:26 +0500

    6.4.1.1. Обход массива с последнего элемента [#425872]

---
+++ src/ru/job4j/array/ReverseLoopForArray.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class ReverseLoopForArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = numbers.length - 1; i >=0 ; i= i-2) {
+            System.out.println("Текущий элемент массива начиная с последнего: " + numbers[i]);
+        }
+    }
+}
